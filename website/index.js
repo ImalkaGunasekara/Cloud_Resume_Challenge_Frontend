7a86d03305dbb78b78a8986ce11d62588f9e56c0
Imalka <gunasekara.iusa@gmail.com>
2024-06-08 16:09:33 +0530

    first

---
+++ website/index.js
@@ -0,0 +1,19 @@
+// Replace 'YOUR_API_GATEWAY_URL' with the actual URL of your API Gateway endpoint
+const apiUrl = 'https://0lpdcjnrvb.execute-api.us-east-1.amazonaws.com/dev/example';
+
+async function fetchViewCount() {
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        document.getElementById('view-counter').textContent = data['body']['views'];
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+        document.getElementById('view-counter').textContent = 'Error fetching view count';
+    }
+}
+
+// Fetch the view count when the page loads
+window.onload = fetchViewCount;
